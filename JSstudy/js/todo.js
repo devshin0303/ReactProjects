bce0966d2c10e2473b92aba570a8137c51772331
devshin0303 <ejrdl0303@gmail.com>
2022-03-18 03:58:59 +0900

    0317 js study

--- JSstudy/js/todo.js
+++ JSstudy/js/todo.js
@@ -1,29 +1,63 @@
-const todoForm = document.getElementById("todoForm");
-const todoInput = document.querySelector("#todoForm input");
-const todoList = document.getElementById("todoList");
+const todoForm = document.getElementById("todoForm"); //form element 가져오기
+const todoInput = document.querySelector("#todoForm input"); //form element 안에 있는 input elemen가져오기
+const todoList = document.getElementById("todoList"); //ul element 가져오기
+
+let toDos = [];
 
 function deleteTodo(event) {
-  const li = event.target.parentElement;
+  const li = event.target.parentNode;
   li.remove();
+  toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
+  saveTodos();
+  // li.remove();
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "x";
   button.addEventListener("click", deleteTodo);
+  todoList.appendChild(li); //ul element 요소안에 li
   li.appendChild(span);
   li.appendChild(button);
-  todoList.appendChild(li); //ul element 요소안에 li
 }
 
 function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value; //newTodo변수를 선언 후 사용자로부터 받은 값을 저장
-  todoInput.value = ""; //input 박스 안의 값을 초기화
-  paintTodo(newTodo); //사용자로부터 입력 받은 값을 argument로 받는다.
+  //input 박스 안의 값을 초기화
+  todoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintTodo(newTodoObj); //사용자로부터 입력 받은 값을 argument로 받는다.
+  saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodos = localStorage.getItem("todos");
+
+if (savedTodos) {
+  const parsedTodos = JSON.parse(savedTodos);
+  toDos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
+
+// parsedTodos.forEach((item) => {                    //arrow function
+//   console.log("this is the turn of", item);
+// ----------------------------------------------------------------------
+// function sayHello() {
+//   console.log("this is the turn of", item);
+// }
+
+// parsedTodos.forEach(sayHello);
