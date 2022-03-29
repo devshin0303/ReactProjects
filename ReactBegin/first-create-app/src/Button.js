0be4c55eaf3d5dec01a8e47cb89100b43fa0cb2b
devshin0303 <ejrdl0303@gmail.com>
2022-03-29 13:31:52 +0900

    studying useEffect

--- ReactBegin/first-create-app/src/Button.js
+++ ReactBegin/first-create-app/src/Button.js
@@ -1,7 +1,11 @@
 import styles from "./Button.module.css";
 
-function Button({ text }) {
-  return <button className={styles.title}>{text}</button>;
+function Button({ text, onClick }) {
+  return (
+    <button onClick={onClick} className={styles.title}>
+      {text}
+    </button>
+  );
 }
 
 export default Button;
