dfb779261ef5285ad7665b0aabc2138d25273b68
devshin0303 <ejrdl0303@gmail.com>
2022-03-30 11:35:11 +0900

    devide movieApp

--- ReactBegin/first-create-app/src/App.js
+++ ReactBegin/first-create-app/src/App.js
@@ -1,93 +1,50 @@
-import Button from "./Button";
-import CoinTracker from "./CoinTracker";
-import styles from "./App.module.css";
+import MovieApp from "./MovieApp";
+import React from "react";
 import { useEffect, useState } from "react";
-
-// function Hello() {
-//   useEffect(() => {
-//     console.log("enter");
-//     return () => console.log("exit");
-//   }, []);
-//   return <h1>Hello</h1>;
-// }
+import PropTypes from "prop-types";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-  const onChange = (event) => {
-    setTodo(event.target.value);
-    // console.log(todo);
-  };
-  const onClick = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodos((currentArray) => [todo, ...currentArray]);
-    setTodo("");
-  };
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>My To Dos ({todos.length})</h1>
-      <form>
-        <input
-          value={todo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button onClick={onClick}>submit</button>
-      </form>
-      <hr />
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-      <CoinTracker />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>MovieApp</h1>
+          {movies.map((movie) => (
+            <MovieApp
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
-  // -------------------------------------------------------------
-  // useEffect 를 활용한 예제
-  // -------------------------------------------------------------
-  // const [counter, setCounter] = useState(0);
-  // const [keyword, setKeyword] = useState("");
-  // const [showing, setShowing] = useState(false);
-
-  // const showHello = () => setShowing((showing) => !showing);
-  // const onClick = () => setCounter((prev) => prev + 1);
-  // const onChange = (event) => setKeyword(event.target.value);
-
-  // console.log("i run all the time");
-
-  // useEffect(() => {
-  //   console.log("I run only once.");
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("I run when 'keyword' changes");
-  // }, [keyword]);
-
-  // useEffect(() => {
-  //   console.log("I run when 'counter' changes");
-  // }, [counter]);
-
-  // return (
-  //   <div>
-  //     <input
-  //       value={keyword}
-  //       onChange={onChange}
-  //       type="text"
-  //       placeholder="Search here..."
-  //     ></input>
-  //     <h1>{counter}</h1>
-  //     <Button text={"click"} onClick={onClick} />
-  //     {showing ? <Hello /> : null}
-  //     <button onClick={showHello}>{showing ? "hide" : "show"}</button>
-  //   </div>
-  // );
-  // -------------------------------------------------------------
 }
+
+MovieApp.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default App;
