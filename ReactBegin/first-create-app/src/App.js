51da29c1134bce5f3c6ff8a64726973f986da0fb
devshin0303 <ejrdl0303@gmail.com>
2022-03-30 16:19:18 +0900

    finish MovieApp

--- ReactBegin/first-create-app/src/App.js
+++ ReactBegin/first-create-app/src/App.js
@@ -1,50 +1,16 @@
-import MovieApp from "./MovieApp";
-import React from "react";
-import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-  }, []);
-
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          <h1>MovieApp</h1>
-          {movies.map((movie) => (
-            <MovieApp
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
-
-MovieApp.propTypes = {
-  coverImg: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  summary: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
 
 export default App;
